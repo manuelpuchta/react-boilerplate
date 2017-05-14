d809bb0968f3df8b980b50e9ba39d5df378545f7
Manuel Puchta <manuel.puchta@gmail.com>
2017-05-14 21:16:46 +0200

    Refactor test data

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('root')
-// );
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import _ from 'lodash';
-
-function component () {
-  var element = document.createElement('div');
-
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
