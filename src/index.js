fcb78dd9168d517358a1b1b3aa7146e9f547ffa6
Manuel Puchta <manuel.puchta@gmail.com>
2017-05-30 12:27:57 +0200

    Refactor asset handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 
 import './index.scss';
 
+import imageDataUrl from '../assets/img/img-data-url.jpg';
+import image from '../assets/img/img.png';
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <h1>Hello, world!</h1>
+    <img src={imageDataUrl} />
+    <img src={image} />
+  </div>,
   document.getElementById('root')
 );
