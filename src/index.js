3dd4347dd5a7c694177035dd969ba2cd005af619
Manuel Puchta <manuel.puchta@gmail.com>
2017-10-16 21:12:17 +0200

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
-
-import imageDataUrl from '../assets/img/img-data-url.jpg';
-import image from '../assets/img/img.png';
+const style = {
+  fontFamily: 'sans-serif',
+  fontSize: '5vw',
+  textAlign: 'center'
+};
 
 ReactDOM.render(
-  <div>
-    <h1>Hello, <span className="icon-html-five"></span> world!</h1>
-    <img src={imageDataUrl} />
-    <img src={image} />
-    <div className="bg"></div>
+  <div style={style}>
+    <h1>Hello, world!</h1>
   </div>,
   document.getElementById('root')
 );
