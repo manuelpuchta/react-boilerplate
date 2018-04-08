9b5113b7efa2d3b32fa1371d07fb79b01d8ffe11
manuelpuchta <manuel.puchta@gmail.com>
2018-04-08 22:48:39 +0200

    Update webpack config, add build related path and clean plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,29 @@ const nodeEnv = process.env.NODE_ENV || 'development';
 const isDevelopment = nodeEnv === 'development';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let plugins = [
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: './public/index.html'
+  })
+];
+
+if (!isDevelopment) {
+  plugins.push(
+    new CleanWebpackPlugin('build')
+  );
+}
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.[hash:4].js',
-    path: path.resolve(__dirname, 'public')
+    path: isDevelopment ? path.resolve(__dirname, 'public') : path.resolve(__dirname, 'build')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html'
-    })
-  ],
+  plugins: plugins,
   module: {
     rules: [
       {
