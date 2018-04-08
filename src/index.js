53a43271db2c17f85956fb41474bf1bcd4d2c980
manuelpuchta <manuel.puchta@gmail.com>
2018-04-08 22:35:24 +0200

    Clean up index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
-
-  render() {
-    const style = {
-      fontFamily: 'sans-serif',
-      fontSize: '5vw',
-      textAlign: 'center'
-    };
-
-    return <h1 style={style}>Hello {this.props.name}</h1>;
-  }
-
-}
-
 ReactDOM.render(
-  <HelloMessage name="World" />,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
