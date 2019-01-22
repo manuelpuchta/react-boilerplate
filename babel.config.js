f2586ce4158c7e06fa8dfe2133b49fb2190e74f1
Manuel Puchta <dev@manuelpuchta.de>
2019-01-22 10:18:08 +0100

    Update babel configuration

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['@babel/preset-env'],
+  env: {
+    development: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties', 'styled-components'],
+    },
+    production: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties', 'styled-components'],
+    },
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties', 'styled-components'],
+    },
+  },
+};
