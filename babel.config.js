260742fa83297d1f0beac54c1f438871b88b18ac
Manuel Puchta <dev@manuelpuchta.de>
2019-06-14 10:35:24 +0200

    Update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   env: {
     development: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
